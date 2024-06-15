6ca9c138955861e309f6799cb540ff55d29841be
Aries Liu <aries.lwb@gmail.com>
2024-06-16 07:41:59 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,9 @@
 #!/usr/bin/env python
-import subprocess
-import os
-import pkg_resources
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-ProjectDir = os.path.dirname(__file__)
-result = subprocess.run(["pip", "install", "basicsr"], capture_output=True, text=True)
-result = subprocess.run(
-    ["pip", "install", "--no-cache-dir", "-U", "openmim"],
-    capture_output=True,
-    text=True,
-)
-result = subprocess.run(["mim", "install", "mmengine"], capture_output=True, text=True)
-result = subprocess.run(
-    ["mim", "install", "mmcv>=2.0.1"], capture_output=True, text=True
-)
-result = subprocess.run(
-    ["mim", "install", "mmdet>=3.1.0"], capture_output=True, text=True
-)
-result = subprocess.run(
-    ["mim", "install", "mmpose>=1.1.0"], capture_output=True, text=True
-)
-
-with open(os.path.join(ProjectDir, "requirements.txt"), "r") as f:
-    requirements = f.read().splitlines()
-requirements = [x for x in requirements if x and not x.startswith("#")]
-requirements = [x.split(" ")[0] if "index-url" in x else x for x in requirements]
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="mmcm",  # used in pip install
@@ -37,13 +14,12 @@ setup(
     # long_description=long_description,
     # long_description_content_type="text/markdown",
     url="https://github.com/TMElyralab/MMCM",
-    packages=find_packages("."),
-    package_dir={"": "."},
     # include_package_data=True,  # please edit MANIFEST.in
+    packages=find_packages(),  # used in import
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=requirements,
+    install_requires=[],
 )
